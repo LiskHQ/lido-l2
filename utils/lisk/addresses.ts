83e849e5f93c3305541426c76f5d097a4a296cbd
Alessandro Ricottone <ricott2@gmail.com>
2024-07-15 15:19:32 +0200

    Add Lisk mainnet deployment scripts

--- utils/lisk/addresses.ts
+++ utils/lisk/addresses.ts
@@ -1,6 +1,11 @@
 import { NetworkName } from "../network";
 import { LiskContractAddresses, CommonOptions } from "./types";
 
+const LiskMainnetAddresses: LiskContractAddresses = {
+  L1CrossDomainMessenger: "0x31B72D76FB666844C41EdF08dF0254875Dbb7edB",
+  L2CrossDomainMessenger: "0x4200000000000000000000000000000000000007",
+};
+
 const LiskSepoliaAddresses: LiskContractAddresses = {
   L1CrossDomainMessenger: "0x857824E6234f7733ecA4e9A76804fd1afa1A3A2C",
   L2CrossDomainMessenger: "0x4200000000000000000000000000000000000007",
@@ -11,6 +16,8 @@ export default function addresses(
   options: CommonOptions = {}
 ) {
   switch (networkName) {
+    case "mainnet":
+      return { ...LiskMainnetAddresses, ...options.customAddresses };
     case "sepolia":
       return { ...LiskSepoliaAddresses, ...options.customAddresses };
     default:
